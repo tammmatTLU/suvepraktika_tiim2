072ed0462ca7fac5ad393159b2afcb0d980aea63
Vaiko <wydvaiko@tlu.ee>
2025-05-20 10:11:27 +0000

    eksam tehud xddd

--- katse_1/src/Pages/AdminAvaleht.tsx
+++ katse_1/src/Pages/AdminAvaleht.tsx
@@ -1,27 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext';
 import LogoutButton from '../components/Logout';
 import ResetColors from '../components/TaastaVarv';
 
 export default function AdminAvaleht() {
+  const [controlCount, setControlCount] = useState(0);
+  const [editCount, setEditCount] = useState(0);
+
+  useEffect(() => {
+    setControlCount(parseInt(localStorage.getItem('controlCount') || '0', 10));
+    setEditCount(parseInt(localStorage.getItem('editCount') || '0', 10));
+  }, []);
+
+  function increaseCount(key, setter) {
+    const current = parseInt(localStorage.getItem(key) || '0', 10);
+    const newValue = current + 1;
+    localStorage.setItem(key, newValue.toString());
+    setter(newValue);
+  }
 
   return (
     <>
-    <ResetColors />
+      <ResetColors />
       <header>
-          <LogoutButton/>
+        <LogoutButton/>
       </header>
       <div className='buttonContainer'>
-      <Link to={'/admin/seadmete-juhtimine'}>
-        <button>Seadmete juhtimine</button>
-      </Link>
-      <Link to={'/admin/automatiseerimine'}>
-        <button>Ajapõhine automatiseerimine</button>
-      </Link>
-      <Link to={'/admin/redigeerimine'}>
-        <button>Kasutajaliidese redigeerimine</button>
-      </Link>
+        <Link to={'/admin/seadmete-juhtimine'}>
+          <button onClick={() => increaseCount('controlCount', setControlCount)}>Seadmete juhtimine</button>
+        </Link>
+        <Link to={'/admin/automatiseerimine'}>
+          <button>Ajapõhine automatiseerimine</button>
+        </Link>
+        <Link to={'/admin/redigeerimine'}>
+          <button onClick={() => increaseCount('editCount', setEditCount)}>Kasutajaliidese redigeerimine</button>
+        </Link>
+      </div>
+      <div style={{ marginTop: 30, fontSize: '16pt', color: 'white', textAlign: 'center' }}>
+        Seadmete juhtimist avatud: {controlCount} korda<br />
+        Kasutajaliidese redigeerimist avatud: {editCount} korda
       </div>
     </>
   )
