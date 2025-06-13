2f00415106ba03ee5a81f2590becdc5b23860a59
msutt <msutt@tlu.ee>
2025-06-13 13:26:25 +0300

    GenerateToken.php update

--- backend/src/Service/GenerateToken.php
+++ backend/src/Service/GenerateToken.php
@@ -2,34 +2,10 @@
 
 namespace App\Service;
 
-use App\Entity\Token;
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
-
 class GenerateToken
 {
-    private EntityManagerInterface $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
+    public function generateToken(): string
     {
-        $this->entityManager = $entityManager;
-    }
-
-    public function generateToken(User $user): string
-    {
-        $tokenValue = bin2hex(random_bytes(32));
-
-        $token = $user->getToken();
-        if (!$token) {
-            $token = new Token();
-            $token->setUser($user);
-            $user->setToken($token);
-        }
-        $token->setValue($tokenValue);
-
-        $this->entityManager->persist($token);
-        $this->entityManager->flush();
-
-        return $tokenValue;
+        return bin2hex(random_bytes(32));
     }
 }
