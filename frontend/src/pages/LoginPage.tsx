7e6da5729b971b0a34feb94d80bd06c305ece209
PeterSaan <saanpeter@gmail.com>
2025-06-17 15:49:06 +0300

    Add fetching and localStorage saving from login form

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,22 +1,68 @@
+import { useState, type FormEvent } from "react";
 import { Link } from "react-router-dom";
 
-
 export default function LoginPage() {
+	interface IErrorMessage {
+		errorMsg: string
+	}
+
+	const apiUrl = import.meta.env.VITE_API_URL;
+
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [errorMsg, setErrorMsg] = useState('');
+
+	async function handleLogin(e: FormEvent) {
+		e.preventDefault();
+		try {
+			const response = await fetch(`${apiUrl}/login`,
+				{
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({ "username": username, "password": password })
+				}
+			);
+
+			if (!response.ok) {
+				throw new Error("Login failed. Try again!");
+			}
+
+			setErrorMsg("");
+			const responseBody = await response.json();
+			localStorage.setItem('userToken', responseBody.token);
+			localStorage.setItem('userName', responseBody.username);
+		}
+		catch (error) {
+			setErrorMsg("Error occured upon login");
+
+			if (error instanceof Error) { setErrorMsg(error.message); }
+		}
+	}
+
+	function ErrorMessage({ errorMsg }: IErrorMessage) {
+		if (errorMsg) {
+			return <h3 className="login-error-msg">{errorMsg}</h3>;
+		}
+	}
+
     return (
         <>
             <div className="login-form">
-                <form action={"localhost:3006/login"} method="post">
+                <form onSubmit={handleLogin}>
                     <div className="form-group">
                         <label htmlFor="username">Kasutajanimi:</label>
-                        <input type="text" id="username" name="username" required />
+                        <input onChange={e => {setUsername(e.target.value)}}
+							type="text" id="username" name="username" required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Parool:</label>
-                        <input type="password" id="password" name="password" required />
+                        <input onChange={e => {setPassword(e.target.value)}}
+							type="password" id="password" name="password" required />
                     </div>
                     <button type="submit" className="btn-grad">Logi sisse</button>
+					<Link to="/action-select"><button className="btn-grad">Testi nupp</button></Link>
                 </form>
-                <Link to="/action-select"><button className="btb-grad">Testi nupp</button></Link>
+				<ErrorMessage errorMsg={errorMsg} />
             </div>
         </>
     );
