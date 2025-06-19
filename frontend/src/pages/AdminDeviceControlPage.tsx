f3d41754ea95d19b8e4d7f5eb8d314e813c0e6cf
Mattias Tamm <tammmat@tlu.ee>
2025-06-19 09:43:12 +0300

    Added loading buffer to admin's central control panel page.

--- frontend/src/pages/AdminDeviceControlPage.tsx
+++ frontend/src/pages/AdminDeviceControlPage.tsx
@@ -3,12 +3,21 @@ import { useEffect } from 'react';
 import { loadButtonTemplates } from "../store/slices/buttonTemplateSlice";
 import { useAppDispatch } from '../store/hooks';
 import AdminDeviceControlPanel from "../components/AdminDeviceControlPanel";
+import { useAppSelector } from "../store/hooks";
 
 export default function AdminDeviceControlPage() {
     const dispatch = useAppDispatch();
     useEffect(() => {
         dispatch(loadButtonTemplates());
     }, [dispatch]);
+
+    const loading = useAppSelector(state => state.buttonTemplate.loading);
+    const error = useAppSelector(state => state.buttonTemplate.error);
+
+    if(loading) return <div>Laeb...</div>;
+    if(error) return <div>Error: {error}</div>;
+
+
     return (
         <div className="grid-layout">
             <header>
